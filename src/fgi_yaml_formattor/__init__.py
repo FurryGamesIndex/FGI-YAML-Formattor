82632faf00040a245ce7864bf9d5805a002ab86e
kaesinol <keiplyer@gmail.com>
2024-12-01 11:28:12 +0800

    :bookmark::sparkles:v0.1.1.post fix __all__

--- src/fgi_yaml_formattor/__init__.py
+++ src/fgi_yaml_formattor/__init__.py
@@ -1,10 +1,9 @@
+__all__ = ["dump_to_yaml", "pss_dedent"]
 from textwrap import dedent
 
 from ruamel.yaml import YAML
 from ruamel.yaml.scalarstring import PreservedScalarString
 from ruamel.yaml.comments import CommentedSeq
-
-__all = ['dump_to_yaml']
 
 
 def pss_dedent(x: str) -> PreservedScalarString:
@@ -15,9 +14,11 @@ fgi = YAML(typ=["rt", "string"])
 fgi.indent(sequence=4, offset=2)
 fgi.preserve_quotes = True
 fgi.width = 4096
-fgi.Emitter.flow_seq_start = '[ '      # defaults to '['
-fgi.Emitter.flow_seq_end = ' ]'        # defaults to ']'
-fgi.Emitter.flow_seq_separator = ' ,'
+fgi.Emitter.flow_seq_start = "[ "  # defaults to '['
+fgi.Emitter.flow_seq_end = " ]"  # defaults to ']'
+fgi.Emitter.flow_seq_separator = " ,"
+
+
 def dump_to_yaml(data: dict) -> str:
     def seq(*_):
         s = CommentedSeq(*_)
@@ -26,8 +27,8 @@ def dump_to_yaml(data: dict) -> str:
 
     data["brief-description"] = pss_dedent(data["brief-description"])
     data["description"] = pss_dedent(data["description"])
-    for i in data['authors']:
-        i['role'] = seq(i['role'])
+    for i in data["authors"]:
+        i["role"] = seq(i["role"])
     temp = fgi.dump_to_string(data)  # type: ignore
     for i in list(data.keys())[1:]:
         temp = temp.replace("\n" + i, "\n\n" + i)
